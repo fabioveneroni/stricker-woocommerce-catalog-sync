254768748dad618361f03adbd9a341a2fcca3720
fabioveneroni <159145140+fabioveneroni@users.noreply.github.com>
2026-08-31 11:25:16 +0100

    chore: bump plugin version to 0.3.2

--- stricker-woocommerce-catalog-sync.php
+++ stricker-woocommerce-catalog-sync.php
@@ -2,12 +2,12 @@
 /**
  * Plugin Name: Stricker WooCommerce Catalog Sync
  * Description: Baixa e processa o catálogo XML da Stricker para posterior sincronização com WooCommerce.
- * Version: 0.3.1
+ * Version: 0.3.2
  * Author: Fabio Veneroni
  * Text Domain: stricker-woocommerce-catalog-sync
  */
 if ( ! defined( 'ABSPATH' ) ) exit;
-define( 'SWCS_VERSION', '0.3.1' );
+define( 'SWCS_VERSION', '0.3.2' );
 define( 'SWCS_FILE', __FILE__ );
 define( 'SWCS_DIR', plugin_dir_path( __FILE__ ) );
 define( 'SWCS_URL', plugin_dir_url( __FILE__ ) );
